81978530e5fc72767755129f8971c49b3f394932
Vincent C <vinch.cm@gmail.com>
2025-11-19 00:52:15 +0100

    Average by student by quarter implemented

--- grades/avg.ts
+++ grades/avg.ts
@@ -2,49 +2,63 @@
 
 import data from "./json/grades.json";
 
-let gradesByStudent: Array<any> = data.students;
+let gradesByQuarter: Array<any> = data.quarters;
 
-function moyenne(gradesByStudent: Array<any>) {
+function moyenne(gradesByQuarter: Array<any>) {
   let studentInfo: Array<any> = [];
   let studentName: string = "";
+  let quarterName: string = "";
   let studentCourses: Array<any> = [];
-  const sentence1: string = "Le détail des moyennes est le suivant : ";
+  let sentence0: string = "";
+  let sentence1: string = "";
   let sentence2: string = "";
 
-  for (let h = 0; h < gradesByStudent.length; h++) {
-    studentName = gradesByStudent[h].name;
-    studentCourses = gradesByStudent[h].courses;
-    let averages: any[] = [];
-    let average: any = 0;
-    for (let i = 0; i < studentCourses.length; i++) {
-      let totalGrades: number = 0;
-      let coeffs: number = 0;
-      let x: Array<any> = studentCourses[i].grades;
-      for (let j = 0; j < x.length; j++) {
-        totalGrades =
-          totalGrades + ((x[j].value * 20) / x[j].denom) * x[j].coeff;
-        coeffs = coeffs + x[j].coeff;
-      }
-      averages.push([
-        studentCourses[i].name,
-        (totalGrades / coeffs).toFixed(2),
-      ]);
-    }
+  for (let g = 0; g < gradesByQuarter.length; g++) {
+    quarterName = gradesByQuarter[g].name;
+    sentence0 = `Voici les résultats du ${quarterName} trimestre : `;
+    studentInfo.push(sentence0);
+    let quarterStudent: Array<any> = gradesByQuarter[g].students;
+    for (let h = 0; h < quarterStudent.length; h++) {
+      studentName = quarterStudent[h].name;
+      studentCourses = quarterStudent[h].courses;
+      let averages: any[] = [];
+      let average: any = 0;
 
-    let y: number = 0;
-    let nan: number = 0;
-    for (let k = 0; k < averages.length; k++) {
-      if (averages[k][1] != "NaN") {
-        y = y + parseFloat(averages[k][1]);
-      } else {
-        nan += 1;
+      for (let i = 0; i < studentCourses.length; i++) {
+        let totalGrades: number = 0;
+        let coeffs: number = 0;
+        let x: Array<any> = studentCourses[i].grades;
+        if (x.length > 1) {
+          for (let j = 0; j < x.length; j++) {
+            totalGrades =
+              totalGrades + ((x[j].value * 20) / x[j].denom) * x[j].coeff;
+            coeffs = coeffs + x[j].coeff;
+          }
+        } else {
+          totalGrades = ((x[0].value * 20) / x[0].denom) * x[0].coeff;
+          coeffs = x[0].coeff;
+        }
+        averages.push([
+          studentCourses[i].name,
+          (totalGrades / coeffs).toFixed(2),
+        ]);
       }
+      let y: number = 0;
+      let nan: number = 0;
+      for (let k = 0; k < averages.length; k++) {
+        if (averages[k][1] != "NaN") {
+          y = y + parseFloat(averages[k][1]);
+        } else {
+          nan += 1;
+        }
+      }
+      average = (y / (averages.length - nan)).toFixed(2);
+      sentence1 = `Le détail des moyennes de ${studentName} pour le ${quarterName} trimestre est le suivant : `;
+      sentence2 = `La moyenne générale de ${studentName} pour le ${quarterName} trimestre est de : ${average} sur 20.`;
+      studentInfo.push(sentence1, averages, sentence2);
     }
-    average = (y / (averages.length - nan)).toFixed(2);
-    sentence2 = `La moyenne générale de ${studentName} est de : `;
-    studentInfo.push(sentence1, averages, sentence2 + average);
   }
   return studentInfo;
 }
 
-console.log(moyenne(gradesByStudent));
+console.log(moyenne(gradesByQuarter));
